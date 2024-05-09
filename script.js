f16d0823433f51ca35a49cc4955f6ee4285288c9
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2024-05-09 21:26:16 +0330

    functionality of rolling dice when it's 1

--- script.js
+++ script.js
@@ -4,8 +4,11 @@ const rollBtn = document.querySelector('.btn--roll');
 const holdBtn = document.querySelector('.btn--hold');
 const current1 = document.querySelector('#current--1');
 const current2 = document.querySelector('#current--2');
+const player1 = document.querySelector('.player--1');
+const player2 = document.querySelector('.player--2');
 
 let currentScore = 0;
+let activePlayer = 1;
 
 dice.classList.add('hidden');
 
@@ -17,8 +20,24 @@ rollBtn.addEventListener('click', function () {
 
     if (diceNumber !== 1) {
         currentScore += diceNumber;
-        current1.textContent = currentScore;
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
     } else {
+        // Switching to the next player
+        document.getElementById(`current--${activePlayer}`).textContent = 0;
+        currentScore = 0;
+        activePlayer = activePlayer === 1 ? 2 : 1;
+        // 1- changing the background color
+        player1.classList.toggle('active');
+        player2.classList.toggle('active');
+
+        // 2- adding overline to the text of new player
+
+        // 3- changing the color of text of new player
+
+        // 4- adding a voice like "ouch!" played for the lost player
+
+        // 5- shaking the current section for lost player
+
 
     }
 });
